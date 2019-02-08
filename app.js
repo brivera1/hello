ccaddee7c01a89e3cac5560911974ffc7249aad8
Brandon <brandonrivera983@gmail.com>
2019-02-07 23:38:15 -0600

    pop

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const app = new express();
 
 app.get('/', function(request, response){
     response.sendfile('index.html');
-    var player = require('play-sound')(opts = {})
-
- player.play('./song.mp3', function (err) {
-   if (err) throw err;
-   console.log("Audio finished");
- });
 });
 
-var server = app.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port;
-    console.log("Express is working on port " + port);
-  });
+app.listen(3000, () => console.log('Server running on port 3000'))
