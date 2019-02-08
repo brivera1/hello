5f28ea1173f5da4777836b652b2ed60cb5d0868c
Brandon <brandonrivera983@gmail.com>
2019-02-07 19:57:15 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server running on port 3000'))
